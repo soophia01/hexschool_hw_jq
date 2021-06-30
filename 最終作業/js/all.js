13672b7b54fb9eb5c69108a93fcc95f99a70adc5
soophia01 <73693378+soophia01@users.noreply.github.com>
2021-07-01 02:48:32 +0800

    Delete 最終作業 directory

--- 最終作業/js/all.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('.menu ul li').click(function (e) { 
-        e.preventDefault();
-
-        $(this).toggleClass('show').siblings().removeClass('show');
-        
-    });
-});
